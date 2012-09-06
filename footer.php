aff1ccf6789fefb0f1c1e9a9ce8ee8a1f722418b
Taylor Lapeyre <taylorlapeyre@gmail.com>
2012-09-06 13:19:28 -0500

    added footer.php

---
+++ footer.php
@@ -0,0 +1,15 @@
+  <footer class="row">
+    <div class="well">
+      <p class="muted">You should follow me on twitter <a href="http://www.twitter.com/taylorlapeyre">here</a></p>
+      <p class="muted">© Taylor Lapeyre 2012</p>
+    </div>
+  </footer>
+
+
+  
+  <!-- js files -->
+  <script src="http://code.jquery.com/jquery-latest.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <!-- /js files -->
+</body>
+</html>
