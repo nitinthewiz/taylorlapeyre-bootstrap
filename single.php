913996f6e157c721928b06f7831ac008957e6371
Taylor Lapeyre <taylorlapeyre@gmail.com>
2012-09-06 13:23:27 -0500

    fixed php bug, minor changes

--- single.php
+++ single.php
@@ -6,7 +6,7 @@
       <div class="span8">
         <h3><?php the_title(); ?></h3>
         <p class="lead"><?php the_time('F jS, Y') ?></p>
-        <?php get_post( $post_id ); ?>
+        <?php the_content('Read more'); ?>
         <br />
         <?php comments_template( '/comments.php', true ); // Loads the comments.php template. ?>
       </div>
