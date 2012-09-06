0e0f0f3e17a51e371e96f1cf862a3737f1891ecc
Taylor Lapeyre <taylorlapeyre@gmail.com>
2012-09-06 18:53:47 -0500

    updated footer

--- footer.php
+++ footer.php
@@ -1,11 +1,10 @@
   <footer class="row">
     <div class="well">
-      <p class="muted">You should follow me on twitter <a href="http://www.twitter.com/taylorlapeyre">here</a></p>
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>/?page_id=22">About Me</a>
       <p class="muted">© Taylor Lapeyre 2012</p>
+      <p class="muted">You should follow me on twitter <a href="http://www.twitter.com/taylorlapeyre">here</a></p>
     </div>
   </footer>
-
-
   
   <!-- js files -->
   <script src="http://code.jquery.com/jquery-latest.js"></script>
